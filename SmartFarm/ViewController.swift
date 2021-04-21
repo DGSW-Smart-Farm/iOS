7efd5e5f159e51be759cdd378ad631b375412dd3
Skyline-23 <bs2740@naver.com>
2021-04-22 08:30:49 +0900

    메인화면 테이블뷰 추가

--- SmartFarm/ViewController.swift
+++ SmartFarm/ViewController.swift
@@ -21,3 +21,19 @@ class ViewController: UIViewController {
     }
 
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+}
