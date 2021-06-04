9b52a257be8b5c49229120ed0d1582ef2837335b
Skyline-23 <bs2740@naver.com>
2021-06-04 22:33:59 +0900

    [ADD] Detail 데이터를 위한 모델 Codable 모델 생성

---
+++ SmartFarm/DeatilView/DetailData.swift
@@ -0,0 +1,20 @@
+//
+//  DetailData.swift
+//  SmartFarm
+//
+//  Created by 김부성 on 2021/06/04.
+//
+
+import Foundation
+
+// DetailData중 LED를 제외한 값을 받아오는 model
+struct DetailData: Decodable {
+    let status: Int
+    let value: Int
+}
+
+// LED data 혼자만 다르기 때문에 JSON decode를 위한 struct를 따로 작성
+struct LedData: Decodable {
+    let status: Bool
+    let time: Int
+}
